57fec5cbad161414287c91bd39d468b792c64a99
Matt Sch <szefler.mateusz91@gmail.com>
2022-05-22 22:42:41 +0200

    Add project files.

---
+++ PhoneBook2/PhoneBook.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook2
+{
+    public class PhoneBook
+    {
+        public List<Contact> Contacts { get; set; } = new List<Contact>();
+
+        // 1
+        public void AddContact(Contact contact)
+        {
+            Contacts.Add(contact);
+        }
+        
+        // 2
+        public void DisplayContactByNumber(string number)
+        {
+            var searchNumber = Contacts.FirstOrDefault(n => n.Number == number);
+            
+            if (searchNumber != null)
+                DisplayContact(searchNumber);
+            else
+                Console.WriteLine("contact not found!");
+        }
+
+        // 3
+        public void DisplayAllContacts()
+        {
+            DisplayContacts(Contacts);
+        }
+
+        // 4
+        public void DisplayContactByPhrase(string searchPhrase)
+        {
+            var matchingPhrase = Contacts.Where(c => c.Name.Contains(searchPhrase)).ToList();
+            DisplayContacts(matchingPhrase);
+        }
+
+        // 5
+        public void DeleteContact(string number)
+        {
+            var deletedContact = Contacts.FirstOrDefault(c => c.Number == number);
+            Contacts.Remove(deletedContact);
+        }
+
+        // auxiliary methods
+        private void DisplayContacts(List<Contact> contacts)
+        {
+            foreach (Contact contact in contacts)
+                DisplayContact(contact);
+        }
+
+        private void DisplayContact(Contact contact)
+        {
+            Console.WriteLine($"{contact.Name}, {contact.Number}");
+        }
+
+        
+    }
+}
---
+++ PhoneBook2/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace PhoneBook2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Mobile phone book app ***");
+
+            Console.WriteLine("Options:");
+            Console.WriteLine("1 - add new contact");
+            Console.WriteLine("2 - display contact by number");
+            Console.WriteLine("3 - display all contacts");
+            Console.WriteLine("4 - search contacts");
+            Console.WriteLine("5 - delete contact");
+            Console.WriteLine("x - exit from app...");
+
+            string userInput = Console.ReadLine();
+
+            PhoneBook phoneBook = new PhoneBook();
+
+            while (true)
+            {
+                switch (userInput)
+                {
+                    case "1":
+
+                        Console.Write("Name of the contact: ");
+                        string name = Console.ReadLine();
+                        Console.Write("Number of the contact: ");
+                        string number = Console.ReadLine();
+
+                        var newContact = new Contact(name, number);
+                        phoneBook.AddContact(newContact);
+                        Console.WriteLine($"{newContact.Name} added.");
+
+                        break;
+                    case "2":
+
+                        Console.Write("Enter a number to display: ");
+                        string numberToSearch = Console.ReadLine();
+                        phoneBook.DisplayContactByNumber(numberToSearch);
+
+                        break;
+                    case "3":
+
+                        Console.WriteLine();
+                        Console.WriteLine("You phone book:");
+                        phoneBook.DisplayAllContacts();
+
+                        break;
+                    case "4":
+
+                        Console.Write("Enter phrase to search for contact: ");
+                        string phraseToSearch = Console.ReadLine();
+                        phoneBook.DisplayContactByPhrase(phraseToSearch);
+
+                        break;
+                    case "5":
+
+                        Console.Write("Enter a contact to delete:");
+                        string deleteContact = Console.ReadLine();
+                        phoneBook.DeleteContact(deleteContact);
+                        Console.WriteLine($"{deleteContact} deleted...");
+
+                        break;
+                    case "x":
+
+                        Console.WriteLine("Closing the app...");
+                        return;
+
+                        break;
+                    default:
+                        Console.WriteLine("choose one of above options");
+                        break;
+                }
+
+                userInput = Console.ReadLine();
+            }
+        }
+    }
+}
